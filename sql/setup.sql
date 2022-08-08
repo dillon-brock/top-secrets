d429bf1966d7dd8a8729fa97d7f4c25f3bd2bd31
Dillon Brock <mdillon1297@gmail.com>
2022-08-08 16:13:33 -0700

    wrote SQL to initialize users and secrets table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT
+);
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO secrets (title, description) VALUES
+('JFK Assassination', 'Shirley Temple killed JFK.'),
+('Trump Rats', 'Donald Trump is actually just 500 rats stacked on top of each other in a trench coat.'),
+('Moon Landing', 'The first moon landing was real, but every subsequent one was staged.');
