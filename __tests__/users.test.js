03896b8f69c0538d1ebdf5c82f9d421dff67c135
Dillon Brock <mdillon1297@gmail.com>
2022-08-08 17:24:21 -0700

    wrote test for signing in user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -18,13 +18,18 @@ describe('authentication and authorization routes', () => {
     const res = await request(app).post('/api/v1/users').send(testUser);
     const { firstName, lastName, email } = testUser;
 
-    // expect(res.status).toBe(200);
+    expect(res.status).toBe(200);
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
       lastName,
       email
     });
+  });
+  it('signs in a user', async () => {
+    const res = await request(app).post('/api/v1/users/session').send(testUser);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.any(String));
   });
   afterAll(() => {
     pool.end();
